9f5723732cd8c39d0a0426d3b81dcf3525827a41
Gonzalo Tapia <correo@gmail.com>
2021-05-17 21:14:02 -0400

    se agrego api

---
+++ js/vin.js
@@ -0,0 +1,36 @@
+const input = document.querySelector('.marca');
+const input2 = document.querySelector('.modelo');
+const input3 = document.querySelector('.año');
+const button = document.querySelector('button2');
+const imagen = document.querySelector('.traer');
+
+button2.addEventListener('click', (e) => {
+    e.preventDefault();
+    traer(input.value, input2.value, input3.value);
+
+})
+function traer(marca, model, año){        
+    fetch(`https://api.carsxe.com/images?key=albv3qkt1_so8tpezkl_466keecr9&make=${marca}&model=${model}&year=${año}&format=json`)
+    .then (response => response.json())
+    .then (data =>{
+        crearimg(data);
+    })
+}
+function crearimg(auto){
+    const img = document.createElement('img');
+    img.id = "img1" ; img.src = auto.images['0'].link;
+
+    const br = document.createElement('br');
+    br.br;
+
+    const button = document.createElement('button');
+    button.button ; button.id = "button1" ; button.type = "Submit";
+
+    const div = document.createElement('div');
+    div.append(img);
+    div.append(br);
+    div.append(button);   
+
+    imagen.append(div);
+} 
+
